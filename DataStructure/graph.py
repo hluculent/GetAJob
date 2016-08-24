49b942e4d4efb80501d47aac2b4d7c9a8a37bc55
Luculent <601994749@qq.com>
2016-08-24 18:36:18 +0800

    graph , BFS and DFS

---
+++ DataStructure/graph.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+比如有这么一张图：
+
+    A -> B
+    A -> C
+    B -> C
+    B -> D
+    C -> D
+    D -> C
+    E -> F
+    F -> C
+可以用字典和列表来构建
+
+ graph = {'A': ['B', 'C'],
+             'B': ['C', 'D'],
+             'C': ['D'],
+             'D': ['C'],
+             'E': ['F'],
+             'F': ['C']}
+"""
+def find_path(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return path
+        if not graph.has_key(start):
+            return None
+        for node in graph[start]:
+            if node not in path:
+                newpath = find_path(graph, node, end, path)
+                if newpath: return newpath
+        return None
+
+def find_all_paths(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return [path]
+        if not graph.has_key(start):
+            return []
+        paths = []
+        for node in graph[start]:
+            if node not in path:
+                newpaths = find_all_paths(graph, node, end, path)
+                for newpath in newpaths:
+                    paths.append(newpath)
+        return paths
+
+def find_shortest_path(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return path
+        if not graph.has_key(start):
+            return None
+        shortest = None
+        for node in graph[start]:
+            if node not in path:
+                newpath = find_shortest_path(graph, node, end, path)
+                if newpath:
+                    if not shortest or len(newpath) < len(shortest):
+                        shortest = newpath
+        return shortest
+
+class Graph(object):
+
+    def __init__(self,*args,**kwargs):
+        self.node_neighbors = {}
+        self.visited = {}
+
+    def add_nodes(self,nodelist):
+
+        for node in nodelist:
+            self.add_node(node)
+
+    def add_node(self,node):
+        if not node in self.nodes():
+            self.node_neighbors[node] = []
+
+    def add_edge(self,edge):
+        u,v = edge
+        if(v not in self.node_neighbors[u]) and ( u not in self.node_neighbors[v]):
+            self.node_neighbors[u].append(v)
+
+            if(u!=v):
+                self.node_neighbors[v].append(u)
+
+    def nodes(self):
+        return self.node_neighbors.keys()
+
+    def depth_first_search(self,root=None):
+        order = []
+        def dfs(node):
+            self.visited[node] = True
+            order.append(node)
+            for n in self.node_neighbors[node]:
+                if not n in self.visited:
+                    dfs(n)
+
+
+        if root:
+            dfs(root)
+
+        for node in self.nodes():
+            if not node in self.visited:
+                dfs(node)
+
+        print order
+        return order
+
+    def breadth_first_search(self,root=None):
+        queue = []
+        order = []
+        def bfs():
+            while len(queue)> 0:
+                node  = queue.pop(0)
+
+                self.visited[node] = True
+                for n in self.node_neighbors[node]:
+                    if (not n in self.visited) and (not n in queue):
+                        queue.append(n)
+                        order.append(n)
+
+        if root:
+            queue.append(root)
+            order.append(root)
+            bfs()
+
+        for node in self.nodes():
+            if not node in self.visited:
+                queue.append(node)
+                order.append(node)
+                bfs()
+        print order
+
+        return order
+
+
+if __name__ == '__main__':
+    g = Graph()
+    g.add_nodes([i+1 for i in range(8)])
+    g.add_edge((1, 2))
+    g.add_edge((1, 3))
+    g.add_edge((2, 4))
+    g.add_edge((2, 5))
+    g.add_edge((4, 8))
+    g.add_edge((5, 8))
+    g.add_edge((3, 6))
+    g.add_edge((3, 7))
+    g.add_edge((6, 7))
+    print "nodes:", g.nodes()
+
+    order = g.breadth_first_search(1)
+    order = g.depth_first_search(1)
