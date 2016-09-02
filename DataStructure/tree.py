65e2a16bf25f9c1b903c25cc085405b1f3d2a333
Luculent <601994749@qq.com>
2016-09-02 09:25:05 +0800

    20160902

---
+++ DataStructure/tree.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from collections import defaultdict
+import json
+
+def tree():
+    return defaultdict(tree)
+
+users = tree()
+users['harold']['username'] = 'hrldcpr'
+users['handler']['username'] = 'matthandlersux'
+
+print(json.dumps(users))
+
+taxonomy = tree()
+taxonomy['Animalia']['Chordata']['Mammalia']['Carnivora']['Felidae']['Felis']['cat']
+taxonomy['Animalia']['Chordata']['Mammalia']['Carnivora']['Felidae']['Panthera']['lion']
+taxonomy['Animalia']['Chordata']['Mammalia']['Carnivora']['Canidae']['Canis']['dog']
+taxonomy['Animalia']['Chordata']['Mammalia']['Carnivora']['Canidae']['Canis']['coyote']
+taxonomy['Plantae']['Solanales']['Solanaceae']['Solanum']['tomato']
+taxonomy['Plantae']['Solanales']['Solanaceae']['Solanum']['potato']
+taxonomy['Plantae']['Solanales']['Convolvulaceae']['Ipomoea']['sweet potato']
+
+def dicts(t):
+    return {k: dicts(t[k]) for k in t}
+
+print(dicts(taxonomy))
